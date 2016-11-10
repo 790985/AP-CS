fd0d2ab54426d35114510b7775117db6942cd6b7
790985 <790985@seq.org>
2016-11-09 22:55:27 -0800

    Update ListRunner.java

--- ListRunner.java
+++ ListRunner.java
@@ -13,7 +13,8 @@ public class ListRunner
     int gpa;
     public static void main(String[] args){
         while(menuNumber() != 4){
-    if(menuNumber() == 1){
+        int input = menuNumber();
+    if(input == 1){
         System.out.println("What is your students name?: ");
         name = sc.next();
         System.out.println("What is your students ID Number?: ");
@@ -21,7 +22,14 @@ public class ListRunner
         System.out.println("What is your students GPA?: ");
         gpa = sc.nextInt();
         addStudent(new Students(name,id,gpa));
-    }
+    }else if (input == 2) {
+       System.out.println("What index is your student at (Start at 0): ");
+       delStudent(sc.nextInt());
+    }else if (input == 3) {
+        printStudents();
+    }else{
+       System.out.println("We do not recognize what you have entered. Please enter a number from 1-4.");
+   }
     
 }
    }
