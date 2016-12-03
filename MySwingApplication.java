a419b769898ebc0bfda708d90fcc8e0f4b0d9520
790985 <790985@seq.org>
2016-12-03 14:52:47 -0800

    Update MySwingApplication.java

--- MySwingApplication.java
+++ MySwingApplication.java
@@ -1,23 +1,18 @@
-/**
- * A very simple Swing application
- * 
- * @Chris Campos 
- * @1.0
- */
 import javax.swing.*;
-public class MySwingApplication implements Runnable
-{
-   JFrame jframe;
-   CanvasComponent cc = new CanvasComponent(50,50);
-   public void run(){
-            jframe = new JFrame("FrameDemo");
-            jframe.setSize(600,600);
-            jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            jframe.add(cc);
-            jframe.setVisible(true);
-    }
-   public static void main() {
-            MySwingApplication mySwingApplicationSwing = new MySwingApplication();
-            javax.swing.SwingUtilities.invokeLater(mySwingApplicationSwing);
-        }
+import java.awt.*;
+public class MySwingApplication implements Runnable{
+	JFrame jframe;
+	public void run(){
+	    CanvasComponent cc = new CanvasComponent(50,50,Color.blue);
+		jframe = new JFrame("MySwingApplication");
+		jframe.setSize(400, 400);
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.add(cc);
+		jframe.setVisible(true);
+		
+	}
+	public static void main() {
+		MySwingApplication mySwingApplicationSwing = new MySwingApplication();
+		javax.swing.SwingUtilities.invokeLater(mySwingApplicationSwing);
+	}
 }
